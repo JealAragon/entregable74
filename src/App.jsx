263f1ab41332efbb13ca92d1245aac9e5ea57db1
Jeal Aragon <jealaragon123@gmail.com>
2023-06-30 12:36:26 -0500

    1.2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Skills from './components/Skills'
-import Projects from './components/projects'
+import Projects from './components/Projects'
 
 function App() {
 
